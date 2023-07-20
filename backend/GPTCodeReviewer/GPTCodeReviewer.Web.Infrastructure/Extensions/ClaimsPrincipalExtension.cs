b21cfe644f9535e40dd8db3e9ff3fa29e0f49b79
RadinTiholov <radin.tiholov@mail.bg>
2023-07-20 20:30:12 +0300

    Add ClaimsPrincipalExtension

---
+++ backend/GPTCodeReviewer/GPTCodeReviewer.Web.Infrastructure/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace GPTCodeReviewer.Web.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.Claims
+                .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)
+                ?.Value;
+        }
+    }
+}
