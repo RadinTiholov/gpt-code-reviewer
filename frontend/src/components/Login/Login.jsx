2839d7b79e86a68a5cb621a6cd12a9265b3a49b2
RadinTiholov <radin.tiholov@mail.bg>
2023-07-21 13:18:06 +0300

    Add login functionality

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,13 +1,61 @@
+import { useState } from "react";
+import * as identityService from "../../dataServices/identityService";
+import { useNavigate } from "react-router-dom";
+
 export const Login = () => {
+
+    const navigate = useNavigate();
+
+    const [inputData, setInputData] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    const [error, setError] = useState({
+        active: false, message: ""
+    });
+
+    const onChange = (e) => {
+        setInputData(state => (
+            { ...state, [e.target.name]: e.target.value }))
+    }
 
     const login = (e) => {
         e.preventDefault();
+
+        identityService.login(inputData)
+            .then(res => {
+                console.log(res)
+                navigate('/')
+            })
+            .catch(res => {
+                setError({ active: true, message: res.message })
+            })
     }
 
     return (
         <form onSubmit={login}>
-            <input type="text" placeholder="Username" /><br />
-            <input type="password" /><br />
+            <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={inputData.username}
+                onChange={onChange}
+            />
+            <br />
+            <input
+                type="password"
+                name="password"
+                value={inputData.password}
+                onChange={onChange}
+            />
+            <br />
+            {error?.active ? 
+            <p>
+                Error!
+            </p>
+            : <></>}
             <button type="submit">Login</button>
         </form>
     )
