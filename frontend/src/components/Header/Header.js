3857ee7403dde361cc73a0cedc00f6397ef32ad7
RadinTiholov <radin.tiholov@mail.bg>
2023-07-22 21:10:36 +0300

    Add header

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo-small-dark.png';
+import { useState } from 'react';
+
+let cx = classNames.bind(styles);
+
+export const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openHeader = () => {
+        setIsOpen((state) => !state)
+    }
+
+    return (
+        <header onClick={() => {return isOpen ? setIsOpen(false) : null}} className={cx('header', isOpen ? '' : 'header-closed')}>
+            <ul className={cx('header-list')}>
+                <li className={cx('header-heading')}>
+                    <img onClick={openHeader} className={cx('header-logo')} src={logo} alt='The logo of the web application' />
+                    <Link className={cx('header-heading-text')} to="/">gpt-code-reviewer</Link>
+                </li>
+                <li>
+                    <i onClick={openHeader} className={cx('header-icon', 'fa-solid', 'fa-play')}></i>
+                    <Link to="/register">Start now</Link>
+                </li>
+                <li>
+                    <i onClick={openHeader} className={cx('header-icon', 'fa-solid', 'fa-right-to-bracket')}></i>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <i onClick={openHeader} className={cx('header-icon', 'fa-solid', 'fa-home')}></i>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <i onClick={openHeader} className={cx('header-icon', 'fa-solid', 'fa-door-open')}></i>
+                    <Link to="/logout">Logout</Link>
+                </li>
+            </ul>
+        </header>
+    );
+};
