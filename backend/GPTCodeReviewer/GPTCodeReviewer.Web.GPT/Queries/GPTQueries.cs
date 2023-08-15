5f06cb41da566ee1b4fed477932273be5c642677
RadinTiholov <radin.tiholov@mail.bg>
2023-08-15 19:18:08 +0300

    Improve query

--- backend/GPTCodeReviewer/GPTCodeReviewer.Web.GPT/Queries/GPTQueries.cs
+++ backend/GPTCodeReviewer/GPTCodeReviewer.Web.GPT/Queries/GPTQueries.cs
@@ -4,7 +4,7 @@
     {
         public static string ReviewCodeQuery(string factors)
         {
-            return $"Please conduct a comprehensive analysis of this code, including bug identification, opportunities for refactoring, and overall code quality assessment. For each aspect, provide detailed feedback with suggestions for improvement and the rationale behind the assigned score. Additionally, determine the programming language used. Return only JSON! I want your answer as JSON in this format: {{ \"language\" : \"your opinion\", \"overallScore\" : \"your score\", \"overallScoreMessage\" : \"your opinion\", {factors} \"overallImprovementMessage\" : \"your message for improvement.\" }}";
+            return $"Please conduct a comprehensive analysis of this code, including bug identification, opportunities for refactoring, and overall code quality assessment. For each aspect, provide detailed feedback with suggestions for improvement and the rationale behind the assigned score. The score should be between 0 and 10.0. Additionally, determine the programming language used. Return only JSON! I want your answer as JSON in this format: {{ \"language\" : \"your opinion\", \"overallScore\" : \"your score\", \"overallScoreMessage\" : \"your opinion\", {factors} }}";
         }
     }
 }
